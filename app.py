d5c1b7f5294aac5318dabb0bf3b9b7bf889107fa
Indranil Sarkar <indranilsarkar811@gmail.com>
2024-10-19 09:47:07 +0530

    Initial commit

--- app.py
+++ app.py
@@ -101,6 +101,3 @@ def about():
     return render_template('about.html')
 
 
-if __name__ == "__main__":
-    app.run(debug=True, port=8000)#we can change the port number like this and
-    #debug=true means if any bug is there show me on browser
---
+++ wsgi.py
@@ -0,0 +1,5 @@
+from app import app
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)#we can change the port number like this and
+    #debug=true means if any bug is there show me on browser
